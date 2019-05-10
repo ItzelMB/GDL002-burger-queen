9e46cc0c80c9e88c040ea365ab89a4f6e81b6dd8
ItzelMB <it.mobec@gmail.com>
2019-05-10 13:14:05 -0500

    Updates

---
+++ precache-manifest.994b2e8e622404e8d45645cd41579834.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadc865775b0a3bc96cf9254268e9cee",
+    "url": "/gdl-burger-queen/index.html"
+  },
+  {
+    "revision": "57f6f0eda78a88f8437c",
+    "url": "/gdl-burger-queen/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/gdl-burger-queen/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "57f6f0eda78a88f8437c",
+    "url": "/gdl-burger-queen/static/js/main.6ae84378.chunk.js"
+  },
+  {
+    "revision": "9d54728fb8d305fa5827",
+    "url": "/gdl-burger-queen/static/js/runtime~main.2a7672b0.js"
+  }
+]);
