779bac635ccbc7306d9b8080c53e028fa3cf2825
ItzelMB <it.mobec@gmail.com>
2019-05-17 12:54:49 -0500

    Add bootstraps and login image

--- src/components/waitersInterf.js
+++ src/components/waitersInterf.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const waitersInterf = () => (
-    <h2>Meseros</h2>
+    <section>
+        <section>
+            <p>User</p>
+            <img alt="logo"></img>
+            <button>VER PEDIDOS</button>
+            <button>Alert</button>
+            <button>X</button>
+        </section>
+        <section>
+            <h2>Platillos</h2>
+            <h2>Bebidas</h2>
+        </section>
+        <aside>
+            <p>Pedidos</p>
+        </aside>
+    </section>
+    
 );
 
 export default waitersInterf;
