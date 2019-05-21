dffb27dc557d778a2df3ebb1c7053ba07a4ec019
ItzelMB <it.mobec@gmail.com>
2019-05-21 13:02:19 -0500

    add bootstraps complements and some menu images

--- src/components/waitersInterf.js
+++ src/components/waitersInterf.js
@@ -1,54 +1,70 @@
 import React from 'react';
 
 const waitersInterf = () => (
-    <section>
+  <section>
 
-        <section className="waitersNav">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <section className="waitersNav">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark waitersNav">
 
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Usuario<span class="sr-only">(current)</span></a>
-                        </li>
-                        <a className="navbar-brand" href="#">Burger Queen Minerva</a>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">VER PEDIDOS</a>
-                        </li>
-                    </ul>
-                </div>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
 
-            </nav>
-        </section>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Usuario<span class="sr-only">(current)</span></a>
+            </li>
+            <a className="navbar-brand" href="#">Burger Queen Minerva</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">VER PEDIDOS</a>
+            </li>
+          </ul>
+        </div>
 
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">A</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">B</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">C</div>
-</div>
-
-
-
-
-        
-
-        <aside>
-            <p>Pedidos</p>
-        </aside>
-
+      </nav>
     </section>
+
+    <ul className="nav nav-tabs" id="myTab" role="tablist">
+      <li className="nav-item">
+        <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">DESAYUNOS</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">COMIDAS</a>
+      </li>
+    </ul>
+    <div className="tab-content" id="myTabContent">
+      <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <h3>PLATILLOS</h3>
+        <img src= {require ('../img/sandwich.png')} alt="sandwich" className="menuIcons"/>
+        <p>sándwich</p>
+        <p>$25.00</p>
+
+        <h3>BEBIDAS</h3>
+        <img src= {require ('../img/coffe.png')} alt="sandwich" className="menuIcons"/>
+        <p>sándwich</p>
+        <p>$25.00</p>
+
+        <h3>INGREDIENTES EXTRAS</h3>
+        <img src= {require ('../img/cheese.png')} alt="sandwich" className="menuIcons"/>
+        <p>sándwich</p>
+        <p>$25.00</p>
+      </div>
+      <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <h3>PLATILLOS</h3>
+          <img src= {require ('../img/hamburger.png')} alt="sandwich" className="menuIcons"/>
+          <p>sándwich</p>
+          <p>$25.00</p>
+
+          <h3>BEBIDAS</h3>
+          <img src= {require ('../img/water.png')} alt="sandwich" className="menuIcons"/>
+          <p>sándwich</p>
+          <p>$25.00</p>
+      </div>
+    </div>
+
+    <aside>
+      <p>Pedidos</p>
+    </aside>
+
+  </section>
 
 );
 
