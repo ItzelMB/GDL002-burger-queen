a4720c7a00e72165e89990e45ee1d1351bb09e1b
ItzelMB <it.mobec@gmail.com>
2019-05-27 18:22:02 -0500

    Change browserRouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login/login';
 import KitchenInterf from './components/KitchenInterf/kitchenInterf';
 import WaitersInterf from './components/WaitersInterf/waitersInterf';
