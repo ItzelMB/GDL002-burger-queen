034df4b1b0629bccf1ade16f2ccf6fa04ac7af6e
ItzelMB <it.mobec@gmail.com>
2019-06-03 12:51:19 -0500

    Build kitchenInterface function: in process

--- src/components/KitchenInterf/kitchenInterf.js
+++ src/components/KitchenInterf/kitchenInterf.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
-const KitchenInterf = () => (
+class KitchenInterf extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+        componentDidMount() {
+
+            const firebaseAccess = this.props.firebase.getRefDb().root.child(this.props.sec);
+            let orderElements = [];
     
-    <div className="kitchenInterf">
-        <h1>COCINA</h1>
-        <Link to={ROUTES.LOGIN}>REGRESAR A INICIO</Link>
-    </div>
-);
+            firebaseAccess.on('value', (snapshot) => {
+                let menuCopy = snapshot.val();
+    
+                for (let menuItem in menuCopy) {
+                    
+                    orderElements.push(
+                        {
+                            client: menuCopy[menuItem].client,
+                            items: menuCopy[menuItem].items,
+                            status: menuCopy[menuItem].status,
+                            total: menuCopy[menuItem].total,
+                        }
+                    )
+                }
+    
+                this.setState({
+                    menu:orderElements
+                })
+            });
+    };
+
+    render() {
+        return (
+            <div className="kitchenInterf">
+                <h1>COCINA</h1>
+                <Link to={ROUTES.LOGIN}>REGRESAR A INICIO</Link>
+            </div>
+        )
+    }
+}
 
 export default KitchenInterf;
